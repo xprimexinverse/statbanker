21280c8f2ec6a19cede520cdba9dc7c2bec64b33
Graeme Walsh <graeme.walsh@hotmail.co.uk>
2024-04-09 12:47:43 +0100

    Update getStatBankData.R
    Added en to URL

--- R/getStatBankData.R
+++ R/getStatBankData.R
@@ -14,7 +14,7 @@ getStatBankData <- function(table,metadata=TRUE,type="px"){
     metadata <- FALSE # I'll switch this back on at a later stage
   }else{
     #data <- pxR::read.px(px_files_urls[grep(table,px_files_urls)]) # May need to add an encoding option here
-    data <- pxR::read.px(paste0("https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/",table,"/PX/2013/"))
+    data <- pxR::read.px(paste0("https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/",table,"/PX/2013/en/"))
   }
 
   # Print metadata
