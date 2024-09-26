db329db9c8e71dbcac2137f726d33cb22ba2d629
Graeme Walsh <graeme.walsh@hotmail.co.uk>
2024-09-26 15:24:50 +0100

    fixed old code: "dat"="value"

--- R/px2col.R
+++ R/px2col.R
@@ -9,7 +9,7 @@
 px2col <- function(px){
   if(!methods::is(px,"px")) stop("Input data is not a px object.")
   data       <- as.data.frame(px)
-  col.data   <- plyr::rename(data, c("dat"="value"))
+  #col.data   <- plyr::rename(data, c("dat"="value"))
   string     <- names(data)
   remove     <- c("Year", "Quarter", "Month", "value","CensusYear","HalfYear")
   z          <- string[! string %in% remove]
